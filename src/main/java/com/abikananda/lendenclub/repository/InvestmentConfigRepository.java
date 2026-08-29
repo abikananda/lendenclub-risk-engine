b35b8946e44d149f69ba7d8acaaa65f23e14e1d3
abikananda <abikananda@users.noreply.github.com>
2026-08-29 08:42:55 +0530

    Add investment config repository

---
+++ src/main/java/com/abikananda/lendenclub/repository/InvestmentConfigRepository.java
@@ -0,0 +1,10 @@
+package com.abikananda.lendenclub.repository;
+
+import com.abikananda.lendenclub.entity.InvestmentConfig;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface InvestmentConfigRepository extends JpaRepository<InvestmentConfig, Long> {
+    Optional<InvestmentConfig> findByLenderIdAndEnabledTrue(Long lenderId);
+}
