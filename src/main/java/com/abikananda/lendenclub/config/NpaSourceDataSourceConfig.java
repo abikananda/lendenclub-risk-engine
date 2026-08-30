cc244fe49479cfdb0788ab2a75725093f4de2763
abikananda <abikananda@users.noreply.github.com>
2026-08-30 12:21:35 +0530

    Keep NPA source datasource isolated from primary JPA datasource

--- src/main/java/com/abikananda/lendenclub/config/NpaSourceDataSourceConfig.java
+++ src/main/java/com/abikananda/lendenclub/config/NpaSourceDataSourceConfig.java
@@ -1,14 +1,12 @@
 package com.abikananda.lendenclub.config;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
-
-import javax.sql.DataSource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Configuration
 @ConditionalOnProperty(prefix = "npa-import", name = "enabled", havingValue = "true")
@@ -20,16 +18,16 @@ public class NpaSourceDataSourceConfig {
         return new DataSourceProperties();
     }
 
-    @Bean(name = "npaSourceDataSource")
-    public DataSource npaSourceDataSource() {
-        return npaSourceDataSourceProperties()
-                .initializeDataSourceBuilder()
-                .build();
-    }
-
     @Bean(name = "npaSourceJdbcTemplate")
-    public JdbcTemplate npaSourceJdbcTemplate(
-            @Qualifier("npaSourceDataSource") DataSource dataSource) {
+    public JdbcTemplate npaSourceJdbcTemplate() {
+        DataSourceProperties properties = npaSourceDataSourceProperties();
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl(properties.getUrl());
+        dataSource.setUsername(properties.getUsername());
+        dataSource.setPassword(properties.getPassword());
+        if (properties.getDriverClassName() != null && !properties.getDriverClassName().isBlank()) {
+            dataSource.setDriverClassName(properties.getDriverClassName());
+        }
         return new JdbcTemplate(dataSource);
     }
 }
